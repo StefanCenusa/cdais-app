48e907276f76e24a2174c9091b707db137be6783
TheMGM <marian.morosac@gmail.com>
2015-03-29 22:11:01 +0300

    Showcase versiunea CP: - added carousel, login & form button, responsive pics + PLM

--- showcase/js/custom.js
+++ showcase/js/custom.js
@@ -1,12 +1,3 @@
-var bmf = false;
-function showBMF(fieldID) {
-    var target = document.getElementById(fieldID);
-    if (bmf) {
-        bmf = false;
-        target.classList.add("hidden"); // hide
-    }
-    else {
-        bmf = true;
-        target.classList.remove("hidden"); //show
-    }
+function makeActive() {
+    this.classlist.add("active");
 }
