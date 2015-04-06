40bd78738ee0946a1b1f77888a3efbad9d73953a
Cezar Manea <czmanea@bitdefender.com>
2015-04-06 08:43:51 +0300

    login, logout and register working with post request. need to implement JSON RPC, token system and think about "remember me" option

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,44 +1,51 @@
 var express = require('express');
 var router = express.Router();
-var jwt = require('jsonwebtoken');
 
 var isAuthenticated = function (req, res, next) {
-	// if user is authenticated in the session, call the next() to call the next request handler 
-	// Passport adds this method to request object. A middleware is allowed to add properties to
-	// request and response objects
-	if (req.isAuthenticated())
-		return next();
-	// if the user is not authenticated then redirect him to the login page
-	res.redirect('/');
+    // if user is authenticated in the session, call the next() to call the next request handler
+    // Passport adds this method to request object. A middleware is allowed to add properties to
+    // request and response objects
+    if (req.isAuthenticated())
+        return next();
+    // if the user is not authenticated then redirect him to the login page
+    res.redirect('/');
 }
 
-module.exports = function(passport){
+module.exports = function (passport) {
 
-	/* Handle Login POST */
-	router.post('/login', function(req, res){
-        passport.authenticate('login', {
-            successRedirect: res.status(200).send('logged in!'),
-            failureRedirect: '/',
-            failureFlash : true
-        })
+    /* Handle Login POST */
+    router.post('/login', function(req, res){
+        passport.authenticate('login', function(err, user, info){
+            if (err) return res.send(err);
+            if (!user)
+                return res.send(info);
+            req.logIn(user, function (err){
+                if (err) return res.send(err);
+                return res.send('OK!');
+            })
+        })(req, res);
     });
 
-	/* Handle Registration POST */
-	router.post('/signup', function(req, res){
-        passport.authenticate('signup', {
-            successRedirect: res.status(200).send('siggned up!'),
-            failureRedirect: res.status(500).send('fail to signup in!'),
-            failureFlash : true
-        })
+    /* Handle Registration POST */
+    router.post('/signup', function(req, res) {
+        passport.authenticate('signup', function (err, user, info) {
+            if (err) return res.send(err);
+            if (!user)
+                return res.send(info);
+            req.logIn(user, function (err) {
+                if (err) return res.send(err);
+                return res.send('OK!');
+            })
+        })(req, res);
     });
 
-	/* Handle Logout */
-	router.get('/signout', function(req, res) {
-		req.logout();
-        res.status(200).send('logged out!');
-	});
+    /* Handle Logout */
+    router.get('/signout', function (req, res) {
+        req.logout();
+        res.redirect('/');
+    });
 
-	return router;
+    return router;
 }
 
 
