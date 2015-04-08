12ebd2f9191da71aaaa3d74f464d5e4d7bf620b4
TheMGM <marian.morosac@gmail.com>
2015-04-08 16:35:41 +0300

    .gitignore updated, commit issue

---
+++ showcase/js/custom.js
@@ -0,0 +1,10 @@
+function makeActive() {
+    this.classlist.add("active");
+}
+
+/*$(function () {
+    $('#sb-search').click(function () {
+        $('#login-button, #login-button').toggle()
+    });
+
+});*/
