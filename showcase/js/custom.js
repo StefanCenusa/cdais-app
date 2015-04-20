01775f9c26990e87bc192c36edd1b9d22ce06dcd
Stefana Popa <stefana.p94@gmail.com>
2015-04-20 20:24:48 +0300

    Fixed sourcetree issue

--- showcase/js/custom.js
+++ showcase/js/custom.js
@@ -1,14 +1,1 @@
-<<<<<<< HEAD
 
-=======
-function makeActive() {
-    this.classlist.add("active");
-}
-
-/*$(function () {
-    $('#sb-search').click(function () {
-        $('#login-button, #login-button').toggle()
-    });
-
-});*/
->>>>>>> 816be1647025663ec6ca31cfc54d41442f318c4a
