6e00a921aaf86bf699fd63c21d773bb113ea801c
Andrei C <andrei.cioromila@gmail.com>
2015-04-21 23:23:39 +0300

    updated Post module + added fonts for glyphicons

--- client/public/app/modules/dashboard-post.js
+++ client/public/app/modules/dashboard-post.js
@@ -1,5 +1,22 @@
 angular.module('DashboardPost', ['ui.bootstrap', 'textAngular'])
 
-    .controller('DashboardPostCtrl', function ($scope) {
+    .controller('DashboardPostCtrl', function ($scope, $http, $location) {
 
+        $scope.preview = false;
+        
+        $scope.uploadBlogpost = function(){
+            //upload to server.
+            $http.post('/api/post', $scope.htmlcontent)
+                .success(function(data){
+                    $location.path('/');
+                })
+                .error(function (error) {
+                    $scope.status = 'Unable to connect' + error.message;
+                });
+            console.log($scope.htmlcontent);
+        };
+
+        $scope.previewBlogpost = function(){
+            $scope.preview = !$scope.preview
+        }
     });
