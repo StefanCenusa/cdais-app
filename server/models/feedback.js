a221104d771f75e3a9d4ec8da3888451106cc1b1
TheMGM <marian.morosac@gmail.com>
2015-06-01 16:11:15 +0300

    organised Models and Schemas, added Feedback(Trainer, Debater), Event and updated User

--- server/models/feedback.js
+++ server/models/feedback.js
@@ -1,11 +1,29 @@
 var mongoose = require('mongoose');
 
 var feedbackFromTrainerSchema = new mongoose.Schema({
-    name: String
+    date: {type: Date, default: Date.now},
+    trainer: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    position: String,
+    score: Number,
+    feedback: String
 });
-
 var feedbackFromDebaterSchema = new mongoose.Schema({
-    name: String
+    date: {type: Date, default: Date.now},
+    debater: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    feedback: [
+        {
+            style: {type: Number, default: 0}
+        },
+        {
+            strategy: {type: Number, default: 0}
+        },
+        {
+            content: {type: Number, default: 0}
+        },
+        {
+            learning: {type: Number, default: 0}
+        }
+    ]
 });
 
 module.exports = mongoose.model('FeedbackFromTrainer', feedbackFromTrainerSchema);
