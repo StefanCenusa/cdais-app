76403a9b4bf2156e2e252a8ea5f378793ea76107
Andrei C <andrei.cioromila@gmail.com>
2015-06-02 17:51:48 +0300

    post to blog flow complete

--- client/public/app/modules/dashboard-post.js
+++ client/public/app/modules/dashboard-post.js
@@ -1,14 +1,21 @@
 angular.module('DashboardPost', ['ui.bootstrap', 'textAngular'])
 
-    .controller('DashboardPostCtrl', function ($scope, $http, $location) {
+    .controller('DashboardPostCtrl', function ($scope, $http, $location, AuthToken) {
+
+        var token = AuthToken.getToken();
 
         $scope.preview = false;
         
         $scope.uploadBlogpost = function(){
-            //upload to server.
-            $http.post('/api/post', $scope.htmlcontent)
+            var body = {
+                title: $scope.blogpost.title,
+                content: $scope.blogpost.htmlcontent,
+                created_byID: $scope.user._id
+            };
+
+            $http({method: 'POST', url: CONFIG.blogpost + '?token=' + token, data: body})
                 .success(function(data){
-                    $location.path('/');
+                    $location.path('/dashboard/home');
                 })
                 .error(function (error) {
                     $scope.status = 'Unable to connect' + error.message;
