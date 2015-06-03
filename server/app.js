1089b00b0a7386cd4b09cfbbe311ac4a2d1a19d4
TheMGM <marian.morosac@gmail.com>
2015-06-03 04:10:03 +0300

    soft-coded Feedback/Trainer/Debaters, added .showUserByField post method

--- server/app.js
+++ server/app.js
@@ -19,11 +19,11 @@ var httpMethodMap = {
         '/user': require('./paths/user').getUser,
         '/group': require('./paths/group').getGroup,
         '/user/debateHistory': require('./paths/user').getDebateHistory,
-        //'/user/list': require('./paths/user').getUsersList
         '/user/detailedDebateHistory': require('./paths/user').getDetailedDebateHistory
     },
     'POST': {
         '/user': require('./paths/user').hello,
+        '/user/list': require('./paths/user').showUsersByFields,
         '/blogpost': require('./paths/blogpost').saveBlogpost,
         '/group': require('./paths/group').createGroup,
         '/notification': require('./paths/notification').sendNotification,
